9b998b31629975d5d9ec9f931fd5b5a94dfc2e56
yangbushi <gyang@inteloom.com>
2019-04-14 23:47:28 -0400

    INCLUDED ALL PARTS.

---
+++ PROJECT4/CALCULAT.cbl
@@ -0,0 +1,15 @@
+       IDENTIFICATION DIVISION.
+       PROGRAM-ID. CALCULAT as "CALCULAT".
+       AUTHOR.    Xiaoyang Miao.
+
+       LINKAGE SECTION
+            01 LS-QUANTITY-IN       PIC 9(4)       VALUE ZERO.
+            01 LS-UNIT-PRICE-IN     PIC 9(4)       VALUE ZERO.
+            01 LS-STOCK-VALUE-TMP   PIC 9(8)       VALUE ZERO.
+       
+       PROCEDURE DIVISION USING
+            LS-QUANTITY-IN, LS-UNIT-PRICE-IN, LS-STOCK-VALUE-TMP.
+            MULTIPLY LS-QUANTITY-IN BY LS-UNIT-PRICE-IN
+            GIVING LS-STOCK-VALUE-TMP.
+            
+       END PROGRAM  CALCULAT.
